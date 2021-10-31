9a16d4508e399d2ed8740ba19c4510bccb68023b
junior.maniusis@gmail.com <junior.maniusis@gmail.com>
2021-10-31 20:05:14 -0300

    limpa o código

--- examples/asteroids1/pipe.cpp
+++ examples/asteroids1/pipe.cpp
@@ -6,32 +6,15 @@
 void Pipes::initializeGL(GLuint program, const Ship &ship, int quantity) {
   terminateGL();
 
-  //   // Start pseudo-random number generator
-  //   m_randomEngine.seed(
-  //       std::chrono::steady_clock::now().time_since_epoch().count());
-
   m_program = program;
   m_colorLoc = abcg::glGetUniformLocation(m_program, "color");
-  // m_rotationLoc = abcg::glGetUniformLocation(m_program, "rotation");
-  // m_scaleLoc = abcg::glGetUniformLocation(m_program, "scale");
   m_translationLoc = abcg::glGetUniformLocation(m_program, "translation");
 
-  //   // Create asteroids
   m_pipes.clear();
   m_pipes.resize(quantity);
 
   m_pipes[0] = createPipe(glm::vec2{0, .9});
   m_pipes[1] = createPipe(glm::vec2{0, -0.9f});
-  // for (auto &pipe : m_pipes) {
-  //   pipe = createPipe();
-
-  //   // pipe.m_translation = ship.m_translation;
-  //   // Make sure the pipe won't collide with the ship
-  //   //   do {
-  //   // pipe.m_translation = {m_randomDist(m_randomEngine),
-  //   //                       m_randomDist(m_randomEngine)};
-  //   //   } while (glm::length(pipe.m_translation) < 0.5f);
-  // }
 }
 
 void Pipes::paintGL() {
@@ -40,20 +23,13 @@ void Pipes::paintGL() {
   for (const auto &pipe : m_pipes) {
     abcg::glBindVertexArray(pipe.m_vao);
 
-    abcg::glUniform4fv(m_colorLoc, 1, &pipe.m_color.r);
     abcg::glUniform1f(m_scaleLoc, 1);
     abcg::glUniform1f(m_rotationLoc, 0);
 
-    // for (auto i : {-2, 0, 2}) {
-    //   for (auto j : {-2, 0, 2}) {
     abcg::glUniform2f(m_translationLoc, pipe.m_translation.x,
                       pipe.m_translation.y);
 
-    // abcg::glDrawArrays(GL_TRIANGLE_FAN, 0, asteroid.m_polygonSides + 2);
     abcg::glDrawArrays(GL_TRIANGLE_STRIP, 0, 5);
-    //   }
-    // }
-
     abcg::glBindVertexArray(0);
   }
 
@@ -70,23 +46,10 @@ void Pipes::terminateGL() {
 Pipes::Pipe Pipes::createPipe(glm::vec2 translation) {
   Pipe pipe;
 
-  //   auto &re{m_randomEngine};  // Shortcut
-
-  // Randomly choose the number of sides
-  //   std::uniform_int_distribution<int> randomSides(6, 20);
-  //   pipe.m_polygonSides = randomSides(re);
-
-  // Choose a random color (actually, a grayscale)
-  //   std::uniform_real_distribution<float> randomIntensity(0.5f, 1.0f);
-  //   pipe.m_color = glm::vec4(1) * randomIntensity(re);
-
   pipe.m_color.a = 1.0f;
-  // pipe.m_scale = scale;
 
   pipe.width = 20;
   pipe.height = .5;
-  // Choose a random direction
-  //   glm::vec2 direction{m_randomDist(re), m_randomDist(re)};
 
   std::vector<glm::vec2> positions({
       glm::vec2{-pipe.width / 2, pipe.height / 2},   // a
@@ -100,29 +63,19 @@ Pipes::Pipe Pipes::createPipe(glm::vec2 translation) {
 
   pipe.m_translation = translation;
 
-  //   const auto step{M_PI * 2 / asteroid.m_polygonSides};
-  //   std::uniform_real_distribution<float> randomRadius(0.8f, 1.0f);
-  //   for (const auto angle : iter::range(0.0, M_PI * 2, step)) {
-  //     const auto radius{randomRadius(re)};
-  //     positions.emplace_back(radius * std::cos(angle), radius *
-  //     std::sin(angle));
-  //   }
-  //   positions.push_back(positions.at(1));
-
-  // Generate VBO
   abcg::glGenBuffers(1, &pipe.m_vbo);
   abcg::glBindBuffer(GL_ARRAY_BUFFER, pipe.m_vbo);
   abcg::glBufferData(GL_ARRAY_BUFFER, positions.size() * sizeof(glm::vec2),
                      positions.data(), GL_STATIC_DRAW);
   abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  // Get location of attributes in the program
+  // obtem os atributos
   GLint positionAttribute{abcg::glGetAttribLocation(m_program, "inPosition")};
 
-  // Create VAO
+  // cria o VAO
   abcg::glGenVertexArrays(1, &pipe.m_vao);
 
-  // Bind vertex attributes to current VAO
+  // assinala os VBOs para o VAO
   abcg::glBindVertexArray(pipe.m_vao);
 
   abcg::glBindBuffer(GL_ARRAY_BUFFER, pipe.m_vbo);
@@ -131,7 +84,7 @@ Pipes::Pipe Pipes::createPipe(glm::vec2 translation) {
                               nullptr);
   abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-  // End of binding to current VAO
+  // encerra o assinalamento
   abcg::glBindVertexArray(0);
 
   return pipe;
@@ -139,19 +92,8 @@ Pipes::Pipe Pipes::createPipe(glm::vec2 translation) {
 
 void Pipes::update(const Ship &ship, float deltaTime) {
   for (auto &pipe : m_pipes) {
-    // printf("pipe{x=%f; y=%f}\n", pipe.m_translation.x, pipe.m_translation.y);
-    // pipe.m_translation -= ship.m_velocity * deltaTime;
     pipe.m_translation.x = ship.m_translation.x;
     pipe.m_translation.y =
         pipe.m_translation.y - (ship.m_velocity.y * deltaTime);
-    // pipe.m_translation += pipe.m_velocity * deltaTime;
-    // pipe.m_translation.x = ship.m_translation.x;
-    // pipe.m_translation.y = ship.m_translation.y;
-
-    // // Wrap-around
-    // if (pipe.m_translation.x < -1.0f) pipe.m_translation.x += 2.0f;
-    // if (pipe.m_translation.x > +1.0f) pipe.m_translation.x -= 2.0f;
-    // if (pipe.m_translation.y < -1.0f) pipe.m_translation.y += 2.0f;
-    // if (pipe.m_translation.y > +1.0f) pipe.m_translation.y -= 2.0f;
   }
 }
